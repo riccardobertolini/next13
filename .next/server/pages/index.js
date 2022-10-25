0910161e415ec33b7b7888ca4b1e971d2ada94ee
Riccardo Bertolini <info@riccardobertolini.net>
2022-10-25 21:27:37 +0200

    Delete .next directory

--- .next/server/pages/index.js
+++
@@ -1,8 +0,0 @@
-Error.stackTraceLimit = 100;
-global.self = global;
-require("./chunks/_11c488.js");
-require("./chunks/pages_index.js");
-require("./chunks/[embedded_modules]_@vercel_turbopack-next_internal_http.ts.js");
-require("./chunks/[embedded_modules]_@vercel_turbopack-next_internal_shims.ts.js");
-require("./chunks/[embedded_modules]_@vercel_turbopack-next_internal_ipc.ts.js");
-require("./chunks/[embedded_modules]_@vercel_turbopack-next_compiled_stacktrace-parser_index.js");
